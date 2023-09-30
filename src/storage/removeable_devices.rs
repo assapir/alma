165ba887485862f7957e26efc65d86a967ca6a07
Assaf Sapir <assaf@tabnine.com>
2023-09-30 10:46:24 +0300

    refactor get removable device

--- src/storage/removeable_devices.rs
+++ src/storage/removeable_devices.rs
@@ -1,6 +1,9 @@
 use anyhow::Context;
 use byte_unit::Byte;
-use std::{fmt, fs};
+use std::{
+    fmt,
+    fs::{self, DirEntry},
+};
 
 #[derive(Debug)]
 pub struct Device {
@@ -26,52 +29,58 @@ fn trimmed(source: String) -> String {
     String::from(source.trim_end())
 }
 
+fn is_removable_device(device: &DirEntry) -> Result<bool, anyhow::Error> {
+    Ok(fs::read_to_string(device.path().join("removable"))
+        .map(|v| v == "1\n")
+        .context("Error querying storage devices")?)
+}
+
+fn get_model(device: &DirEntry) -> Result<String, anyhow::Error> {
+    Ok(fs::read_to_string(device.path().join("device/model"))
+        .map(trimmed)
+        .context("Error querying storage devices")?)
+}
+
 pub fn get_storage_devices(allow_non_removable: bool) -> anyhow::Result<Vec<Device>> {
-    let mut result = Vec::new();
+    Ok(fs::read_dir("/sys/block")
+        .context("Error querying storage devices")?
+        .filter_map(|entry| {
+            let entry = entry.context("Error querying storage devices").ok()?;
 
-    for entry in fs::read_dir("/sys/block").context("Error querying storage devices")? {
-        let entry = entry.context("Error querying storage devices")?;
+            if !allow_non_removable && !is_removable_device(&entry).ok()? {
+                return None;
+            }
 
-        let removable = allow_non_removable
-            || fs::read_to_string(entry.path().join("removable"))
-                .map(|v| v == "1\n")
-                .context("Error querying storage devices")?;
+            let model = get_model(&entry).ok()?;
+            if model == "CD-ROM" {
+                return None;
+            }
 
-        if !removable {
-            continue;
-        }
-
-        let model = fs::read_to_string(entry.path().join("device/model"))
-            .map(trimmed)
-            .context("Error querying storage devices")?;
-
-        if model == "CD-ROM" {
-            continue;
-        }
-
-        result.push(Device {
-            name: entry
-                .path()
-                .file_name()
-                .expect("Could not get file name for dir entry /sys/block")
-                .to_string_lossy()
-                .into_owned(),
-            model,
-            vendor: fs::read_to_string(entry.path().join("device/vendor"))
-                .map(trimmed)
-                .context("Error querying storage devices")?,
-            size: Byte::from_bytes(
-                fs::read_to_string(entry.path().join("size"))
-                    .context("Error querying storage devices")?
-                    .trim()
-                    .parse::<u128>()
-                    .context("Could not parse block size to unsigned integer (u128)")?
-                    * 512,
-            ),
-        });
-    }
-
-    Ok(result)
+            Some(Device {
+                name: entry
+                    .path()
+                    .file_name()
+                    .expect("Could not get file name for dir entry /sys/block")
+                    .to_string_lossy()
+                    .into_owned(),
+                model,
+                vendor: fs::read_to_string(entry.path().join("device/vendor"))
+                    .map(trimmed)
+                    .context("Error querying storage devices")
+                    .ok()?,
+                size: Byte::from_bytes(
+                    fs::read_to_string(entry.path().join("size"))
+                        .context("Error querying storage devices")
+                        .ok()?
+                        .trim()
+                        .parse::<u128>()
+                        .context("Could not parse block size to unsigned integer (u128)")
+                        .ok()?
+                        * 512,
+                ),
+            })
+        })
+        .collect())
 }
 
 #[cfg(test)]
